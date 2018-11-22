dc4ab187ccf0dad3e21c32ed47c6137f5ab3069d
Petr Houška <houskape@gmail.com>
2018-11-22 17:31:52 +0100

    Clenup.

--- task_02.py
+++ task_02.py
@@ -14,16 +14,22 @@ from petnet.nn import NeuralNet
 from petnet.layers import Linear, Tanh, Sigm
 from petnet.data import BatchIterator, GenIterator, Epoch
 
-def convert_to_bin_array(im, treshold=100):
+# Tranforms image to 2D binary array with custom B/W threshold
+def image_to_bin_array(im, treshold=100):
     im_arr = np.array(im)
     im_arr = np.where(im_arr > treshold, 1, 0)
     
     return im_arr
 
+# Cuts tile out of image
 def cut_tile(im, x, y, size_x, size_y):
     box = (x, y, x + size_x, y + size_y)
     return im.crop(box)
 
+# Cuts image to tiles of specified size 
+# ..could be made significantly more performant through 
+# ..not cutting one tile out of the image at a time
+# ..but cutting the whole image at the same time.
 def cut_to_tiles(im, size_x, size_y):
     tiles = []
     w, h = im.size
@@ -34,26 +40,27 @@ def cut_to_tiles(im, size_x, size_y):
 
     return tiles
 
+# Converts image to flat binary array
 def tile_to_flat_array(im):
-    im_arr = convert_to_bin_array(im)
+    im_arr = image_to_bin_array(im)
     return im_arr.flatten()
 
+# Converts binary array to 8bit B/W image
 def flat_array_to_tile(im_arr, w, h):
     im_arr_shaped = np.reshape(im_arr, (w, h)) * 255
     return Image.fromarray(im_arr_shaped.astype("int8"), "L")
 
+# Merges list of tiles to one picture
 def tiles_to_pic(tiles, w, h):
     new_im = Image.new('L', (w, h))
     tile_w, tile_h = tiles[0].size
-    in_row = w / tile_w
+    tiles_in_row = w / tile_w
 
-    i = 0
-    for tile in tiles:
-        x = i % in_row
-        y = i // in_row
+    for i in range(len(tiles)):
+        x = i % tiles_in_row
+        y = i // tiles_in_row
 
-        new_im.paste(tile, (int(x*tile_w),int(y*tile_h)))
-        i += 1
+        new_im.paste(tiles[i], (int(x*tile_w),int(y*tile_h)))
 
     return new_im
 
