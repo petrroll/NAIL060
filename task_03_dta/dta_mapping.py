dad653f4347c7b965558100f88ee732874658a30
Petr Houška <houskape@gmail.com>
2018-12-11 16:48:56 +0100

    Improve data script.

--- task_03_dta/dta_mapping.py
+++ task_03_dta/dta_mapping.py
@@ -1,27 +1,49 @@
 import os 
 from os.path import isfile, join
 
-index_to_char = {
+# Tile picture names are in format "<charIndex>_<whatever>"
+# ..where charIndex specifies which char should the tile be 
+# ..resolved to in picture->ASCII translation. 
+
+# Mapping of charIndex -> specicif ASCII character
+char_i_to_char = {
     "1":"|",
     "2":"_",
     "3":"/",
 }
 
-dir_path = os.path.dirname(os.path.realpath(__file__))
-pic_names = [f for f in os.listdir(dir_path) if isfile(join(dir_path, f)) if len(f) > 4 and f[-4:] == ".png"]
+def get_picture_paths():
+    '''
+    Returns full paths of all pictures in current folder.
+    '''
+    dir_path = os.path.dirname(os.path.realpath(__file__))
+    pic_names = [f for f in os.listdir(dir_path) if isfile(join(dir_path, f)) if len(f) > 4 and f[-4:] == ".png"]
+
+    return pic_names
+
+def get_data():
+    '''
+    Returns triple of (char_i_to_char, char_i_to_files, file_to_char_i)
+    
+    char_i_to_char: Map charIndex -> ASCII character
+    char_i_to_files: Map charIndex -> list of full file paths
+    file_to_char_i: Map file path -> charIndex
+    '''
+    dir_path = os.path.dirname(os.path.realpath(__file__))
+    pic_names = get_picture_paths()    
+
+    char_i_to_files = {}
+    file_to_char_i = {}
+    for f in pic_names:
+        char_index, _ = f.split("_")
+        file_name_full = join(dir_path, f)
+        if char_index in char_i_to_files:
+            char_i_to_files[char_index].append(file_name_full)
+        else:
+            char_i_to_files[char_index]= [file_name_full]
+        file_to_char_i[file_name_full] = char_index
+
+    return (char_i_to_char, char_i_to_files, file_to_char_i)
 
 
-char_i_to_file = {}
-file_to_char_i = {}
-for f in pic_names:
-    char_index, _ = f.split("_")
-    file_name_full = join(dir_path, f)
-    if char_index in char_i_to_file:
-        char_i_to_file[char_index].append(file_name_full)
-    else:
-        char_i_to_file[char_index]= [file_name_full]
-    file_to_char_i[file_name_full] = char_index
-
-
-print(char_i_to_file)
-print(file_to_char_i)
+print(get_data())
